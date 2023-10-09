2f48b9d8410325b0d8ccd329df78e17098701994
Alex Daniel <adanie02@uoguelph.ca>
2023-10-08 20:10:40 -0400

    Didn't bother to make it fast, but used bubble sort

---
+++ medianOfTwoSortedArrays/README.txt
@@ -0,0 +1,12 @@
+Name: Alex Daniel
+Date: October 8, 2023
+To Run: 
+- make
+- ./medianSort
+
+Description:
+The two arrays are combined into one, then bubble sort is used to sort the array. After that it's determined if the array
+has an even or odd number of elements. Finally, the basic math is done to recieve the median.
+
+LeetCode Description:
+Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
---
+++ medianOfTwoSortedArrays/medianSort.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+double findMedianSortedArrays(int* nums1, int nums1Size, int* nums2, int nums2Size);
+int* bubbleSort(int* arr, int size);
+
+int main() {
+    int* nums1 = malloc(2 * sizeof(int));
+    nums1[0] = 1;
+    nums1[1] = 3;
+    int* nums2 = malloc(2 * sizeof(int));
+    nums2[0] = 2;
+    nums2[1] = 4;
+    int nums1Size = 2, nums2Size = 2;
+    double median = findMedianSortedArrays(nums1, nums1Size, nums2, nums2Size);
+    free(nums1);
+    free(nums2);
+    return 0;
+}
+
+double findMedianSortedArrays(int* nums1, int nums1Size, int* nums2, int nums2Size) {
+    int medianSize = nums1Size + nums2Size;
+    int* medianArray = malloc((medianSize) * sizeof(int));
+    double median = 0;
+    int middle = 0;
+    //Initalize the values of the array
+    for (int i = 0; i < nums1Size; i++) {
+        medianArray[i] = nums1[i];
+    }
+    for(int i = nums1Size; i < medianSize; i++) {
+        medianArray[i] = nums2[i-nums1Size];
+    }
+    medianArray = bubbleSort(medianArray, medianSize - 1);
+    middle = medianSize / 2;
+    //If the array has an odd amount of numbers
+    if (medianSize % 2 == 1) {
+        median = medianArray[middle];
+    }
+    //If the array has an even amount of numbers
+    else {
+        median = (double)(medianArray[middle-1] + medianArray[middle]) / 2.0;
+    }
+    free(medianArray);
+    return median;
+}
+
+//Examines each set of adjacent numbers and switches their positions if they're out of order
+int* bubbleSort(int* arr, int size) {
+    int temp = 0;
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            if (arr[j] > arr[j+1]) {
+                temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+    return arr;
+}
