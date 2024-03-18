547ebd9e1f423a6c3a7d1119f86aa0c70753e28d
Alex Daniel <4lexdaniel@gmail.com>
2024-03-17 22:07:06 -0400

    Sliding Window Technique

--- numSubArraysWithSum/README.md
+++ numSubArraysWithSum/README.md
@@ -6,10 +6,14 @@ Date: March 14, 2024
 - python numSubarraysWithSum.py
 
 ### Results
-
+Speed: 225 ms and beats 20.21% of users
+Memory: 12.50 MB and beats 94.00% of users
 
 ### Description
+The code uses the sliding window technique to efficiently count the number of subarrays whose sum is at most equal to a given goal. It then calculates the number of subarrays with a sum exactly equal to the goal by subtracting the counts of subarrays with a sum less than the goal from the counts of subarrays with a sum at most equal to the goal.
 
+## Notes
+- This approach is less efficient for solving the problem; I was primarily curious about mastering the sliding window technique rather than relying on hash tables.
 
 ### LeetCode Description
 Given a binary array nums and an integer goal, return the number of non-empty subarrays with a sum goal.
--- numSubArraysWithSum/numSubArraysWithSum.py
+++ numSubArraysWithSum/numSubArraysWithSum.py
@@ -1,20 +1,27 @@
 class Solution(object):
-    def numSubarraysWithSum(self, nums, goal):
+    # Calculates the number of subarrays with a sum at most equal to the goal
+    def atMost(self, nums, goal):
         l = 0
-        goalSums = 0
-        currSum = 0
-        for r in range(len(nums)):
-            currSum += nums[r]
-            while currSum > goal:
-                currSum -= nums[l]
+        r = 0
+        ans = 0
+        total_sum = 0
+        length = len(nums)
+        # Iterate through the array with a right pointer
+        while r < length:
+            total_sum += nums[r]
+            # If the sum is larger than the goal, slide the left pointer
+            while total_sum > goal and l <= r:
+                total_sum -= nums[l]
                 l += 1
-            if goal == 0:
-                goalSums += r - l + 1
-            elif currSum == goal:
-                goalSums += 1
-                while l <= r and nums[l] == 0:
-                    goalSums += 1
-                    l += 1
-        return goalSums
+            # Add all subarrays from the right to the left pointer
+            ans += r - l + 1
+            r += 1
+        return ans
+    
+    # Calculates the number of subarrays with a sum exactly equal to the goal
+    def numSubarraysWithSum(self, nums, goal):
+        return self.atMost(nums, goal) - self.atMost(nums, goal - 1)
     
 print(Solution().numSubarraysWithSum([0,0,0,0,1,0,0,0,0,0,1,1,0,0,0,0,0,1,0,0], 3)) # 48
+print(Solution().numSubarraysWithSum([1,0,0,0,0,0,0,0,0,0], 1)) # 10
+print(Solution().numSubarraysWithSum([1,0,1,0,1], 2)) # 4
